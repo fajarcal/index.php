6797b5b899edb0f1229f1dc4ef06c9fc26e6031c
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-03-26 23:32:59 +0700

    first commit

---
+++ htdocs/index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<style>
+body {
+  background-image: url("https://i.postimg.cc/hGCWWxYW/4.png");
+}
+</style>
+<style>
+ul {
+  list-style-type: none;
+  margin-left:300px;
+  padding: 0;
+  overflow: hidden;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+</style>
+</head>
+<body>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#about">About</a></li>
+  <li><a href="#projects">Projects</a></li>
+  <li><a href="#article">Article</a></li>
+</ul>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=10.0">
+    <title>Portofolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        .social-icons {
+            position: fixed;
+            top: 10px;
+            right: 20px;
+            display: flex;
+            gap: 30px;
+        }
+
+        .social-icons a {
+            text-decoration: none; 
+            color: blue; 
+            padding: 10px; 
+            border-radius: 50%; 
+            background-color: #333; 
+            transition: background-color rgba(255, 0, 0, 0.5)s; 
+        }
+
+        .social-icons a:hover {
+            background-color: #FFFFFF; 
+        }
+    </style>
+    <div class="social-icons">
+        <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+    </div>
+<style>
+p {
+  color: white;
+  text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;
+  font-family: 'Times New Roman', sans-serif;
+}
+</style>
+<style>
+p1{
+  color: white;
+  font-family: 'Garamond', Serief;
+  margin-left: 700px;
+  position: relative;
+  top: -40px;
+}
+p2{
+  color: white;
+  font-family: 'Garamond', Serief;
+  margin-left: 700px;
+  position: relative;
+  top: -40px;
+}
+p3{
+  color: white;
+  font-family: 'Garamond', Serief;
+  margin-left: 700px;
+  position: relative;
+  top: -40px;
+}
+</style>
+<style>
+p {
+  font-size: 65px;
+  margin-left:700px;
+  margin-top: 100px;
+  line-height: 01;
+}
+</style>
+<p>Turning Vision Into Reality With Code And Design.</p>
+<div.class="container">
+<p1>As a skilled full-stack developer, I am dedicated to turning ideas into</p1>
+<p2>innovative web applications. Explore my latest projects and articles</p2>
+<p3>Showcasing my expertise in React.js and web development.</p3>
+<style>
+   body {
+      background-color: #FFFFFF; 
+      position: relative; 
+      height: 100vh; 
+   }
+  .button-container {
+      position: absolute; 
+      bottom: 20px; 
+      right: 20px; 
+      display: flex;
+      gap: 10px;
+      left: 700px;
+      bottom: 170px;
+   }
+  .button {
+      background-color: #FFFFFF; 
+      color: black; 
+      padding: 10px 20px; 
+      border: none; 
+      border-radius: 5px; 
+      cursor: pointer; 
+      text-decoration: none; 
+      transition: background-color 0.3s; 
+   }
+  .button:hover {
+      background-color: #555555; 
+      }
+  .button.contact {
+      background-color: transparent; 
+      color: white; 
+      border: 1px solid white; 
+      }
+  .button.contact:hover {
+      background-color: hsla(0, 0%, 100%, 0.5); 
+      color: black; 
+        }
+</style>
+<body>
+    <div class="button-container">
+        <a href="link-to-resume.pdf" class="button" target="_blank">Resume</a>
+        <a href="mailto:your-email@example.com" class="button contact">Contact</a>
+    </div>
+</body>
+<style>
+.flex-container {
+    display: flex; 
+    align-items: center; 
+    justify-content: space-between;
+    position: absolute;
+}
+.image-container {
+    position: absolute;
+    right: 10px; 
+    top: 50%; 
+    transform: translateY(-50%); 
+}
+.gif-animation {
+    width: 210px; 
+    height: auto; 
+    position: absolute;
+    margin-top: -20px;
+    margin-left: 95px;
+}
+</style>
+<body>
+    <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGRtN2pzY2xqdDFhdzExd256YTM5Zjk0MHFraDFlemE0cmdrcGRlcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/k2c4nYjt0jzdy43qni/giphy.gif" alt="Animasi GIF" class="gif-animation" />
+</body>
+    <style>
+.container {
+    display: flex; 
+    align-items: center; 
+    padding: 20px; 
+}
+
+.image-container {
+    margin-right: 750px; 
+}
+
+.image-container img {
+    width: 550px; 
+    height: auto; 
+}
+
+.text-container {
+    max-width: 600px; 
+}
+    </style>
+<body>
+    
+    <div class="image-container">
+        <img src="https://i.postimg.cc/yYsJMGqm/Fotoq.png" alt="Deskripsi Gambar PNG" />
+    </div>
+
+</body>
+<style>
+        body {
+            padding: 0;
+            height: 100vh;
+        }
+        .gif-container {
+            position: absolute; 
+            bottom: 20px; 
+            left: -95px; 
+            width: 150px;
+            top: 470px; 
+            
+        }
+        .gif {
+            width: 100%; 
+            height: auto; 
+        }
+    </style>
+</head>
+<body>
+    <div class="gif-container">
+        <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWphM2I0ZXNxa3l6ejd2czFkb3kwZWV6b3E0NGdxaDFmYTZveTQwZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/mgUSbGSkRIOYipsDNh/giphy.gif" alt="Animasi GIF" class="gif-animation" alt="Animasi" class="gif" />
+    </div>
+</body>
+
+
+
+   
+
+
+
+
+
+
+
